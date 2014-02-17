cb00509c84fefdff35fff8a0ab4c3c70fe499594
denimmart <denimmart@gmail.com>
2014-02-17 14:53:33 +0400

    DB/Quest: Hack fix Relic of the Earthen Ring

---
+++ sql/updates/world/q14100q14111.sql
@@ -0,0 +1,3 @@
+-- Hack fix Relic of the Earthen Ring
+UPDATE `quest_template` SET `RewardItemId1` = 46978, `RewardItemCount1` = 1 WHERE `Id` = 14100;
+UPDATE `quest_template` SET `RewardItemId1` = 46978, `RewardItemCount1` = 1 WHERE `Id` = 14111;
